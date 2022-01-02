0e0765316a03cae761ad2acc2d56267d3edb2dfb
Ashly Thomas Abraham <ashlythomas@gmail.com>
2022-01-02 19:36:35 +0400

    'routing-modified'

--- src/personalprofile.js
+++ src/personalprofile.js
@@ -7,7 +7,7 @@ class PersonalProfile extends Component {
                 <h4>Personal Profile</h4>
 
                 <div className="ContentInside">
-                I am a software architect with holistic knowledge of project architecture, project life cycle and designs. I have 17+ years of working experience in software industry as a full stack developer. I am very passionate in responsive web designs and user experience. Well experienced in coordinating with the product management and stake holders. I have very good analytical and trouble shooting skills. I am an enthusiastic, self-motivated, reliable, responsible and hardworking person. I am a punctual and motivated individual who is able to work in a busy environment and produce high standards of work. I am an excellent team worker and am able to take instructions from all levels and build up good working relationships with all colleagues.
+                I am a full stack developer with holistic knowledge of project architecture, project life cycle and designs. I have 17+ years of working experience in software industry as a full stack developer. I am very passionate in responsive web designs and user experience. Well experienced in coordinating with the product management and stake holders. I have very good analytical and trouble shooting skills. I am an enthusiastic, self-motivated, reliable, responsible and hardworking person. I am a punctual and motivated individual who is able to work in a busy environment and produce high standards of work. I am an excellent team worker and able to take instructions from all levels and build up good working relationships with all colleagues.
                 </div>
             </div>
           );
